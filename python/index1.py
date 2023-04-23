0895668d3e3a81c7684efca775e0692111c266a0
unknown <utkarshup75@gmail.com>
2023-04-23 11:18:58 +0530

    is and is not operator

---
+++ python/index1.py
@@ -0,0 +1,67 @@
+n = 10
+if n > 5:
+    print("n is greater than 5")
+else:
+    print("n is not greater than 5")
+
+
+
+if n == 10: 
+    if n > 5:
+        print("n is a good number")
+
+# if elif else
+if (n == 10):
+    print("if statement")
+elif (n >= 0):
+    print("n is greater than equal to 0")
+else:
+    print("n does not meet the condition ")  
+
+# try except
+try:
+    something = "try block"
+except:
+    ifError = "error here" 
+
+# function 
+
+def firstfunction():
+    print("Hello World function")
+firstfunction()
+
+# function + return statement + parameter/arguments of function 
+# when a function don't returns a value we can it "void"
+
+def returnfunction(a,b):
+    return a+b 
+
+value = returnfunction(1,2)
+print(returnfunction(5,5))
+print(value)
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+     
+
+
+
+    
+
+
+
+
+   
---
+++ python/iterationAndLoop2.py
@@ -0,0 +1,71 @@
+# while loop and break statement 
+n = 0
+while True:
+    if n==3:
+        break
+    print(n)
+    n = n+1
+
+# for in loop, Definite loops 
+
+arr = [1, 2, 3, 4, 5] 
+for key in arr:
+    print(key)
+
+
+# largest number in a array 
+arr = [9, 41, 12, 3, 74, 15]
+largest = -1
+for key in arr:
+    if  key > largest:
+        largest = key 
+print("The largest number is",largest)
+
+# print with serial number 
+
+start = 0
+for key in arr:
+    start = start + 1
+    print(start, key)
+print("The current value of start",start)
+
+# find sum of all the items in array 
+
+sum = 0
+for key in arr:
+    sum += key
+print("The sum is",sum)
+
+# fine average in a loop 
+
+sum = 0
+count = 0
+print("length of arr is",len(arr))
+
+for key in arr:
+    sum += key 
+    count += 1
+print("The average is",sum/count)
+
+# find number greater than 20 filtering 
+
+for key in arr:
+    if key > 20:
+        print("Large number", key)
+    if key == 3:
+        print("the specific number is found",key == 3, key)   
+
+
+# "is" or == and "is not" or != 
+n = 1
+print(n is 1)
+print(n is not 1)
+
+
+
+
+
+
+
+
+
