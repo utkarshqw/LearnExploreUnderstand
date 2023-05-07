ce32a1ed8ce79eaa6a9a912adc9601604c67ba20
unknown <utkarshup75@gmail.com>
2023-05-07 19:53:35 +0530

    string method done

---
+++ python/bro_code/string_method.py
@@ -0,0 +1,15 @@
+# len() length of string 
+# .find() find index of value  
+
+
+name = "nobita"
+print(len(name)) 
+print(name.find("o"))
+print(name.capitalize()) 
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("i"))
+print(name.replace("o", "a"))
+print(name*2) # print something multiple times 
