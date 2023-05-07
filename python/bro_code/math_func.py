8f9d2fbabfd502c786c242ce1b31b6d524e55dea
unknown <utkarshup75@gmail.com>
2023-05-07 22:07:33 +0530

    math function completed

---
+++ python/bro_code/math_func.py
@@ -0,0 +1,15 @@
+import math 
+pi = 3.14
+x = 1
+y = 2 
+z = 3
+
+print(round(pi)) # round figure 
+print(math.ceil(pi)) # smallest integer >= current number 
+print(math.floor(pi))
+print(abs(pi)) # global 
+print(pow(pi, 2))
+print(math.sqrt(420))
+print(max(x, y, z)) 
+print(min(x,y,z))
+
