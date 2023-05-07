a314b1e77671bd9ab3e75d2dc969a823fae6ecd7
unknown <utkarshup75@gmail.com>
2023-05-07 20:34:37 +0530

    type casting done

---
+++ python/bro_code/type_cast.py
@@ -0,0 +1,29 @@
+# type casting = convert the data type of a value to another data type. 
+
+x = 1 #int
+y = 2.0  #float
+z = "3" #str
+
+y = int(y)
+
+print(x)
+print(y)
+print(z*3) # z is str o/p "333"
+
+z = int(z)
+print(z*3) 
+
+x = float(x) 
+print(x)
+
+# int to str 
+print("x is: ", str(int(x))) 
+
+
+
+
+
+
+
+
+
