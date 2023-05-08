2902cc02989982f68d48ab30a8201b435e81270d
unknown <utkarshup75@gmail.com>
2023-05-08 22:32:02 +0530

    for loop

---
+++ python/bro_code/for_loop.py
@@ -0,0 +1,16 @@
+import time
+
+# for i in range(10):
+#     print(i) 
+
+# for i in range(50, 100+1, 2): # 100+1 because 100 101 will be exclusive
+#     print(i)
+
+# for i in "Utkarsh Gupta":
+#     print(i)
+
+for seconds in range(10, 0, -1):
+    print(seconds)
+    time.sleep(1) 
+print("Happy New year")    
+
