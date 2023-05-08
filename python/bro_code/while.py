9a48b37e2681b5a7a800297b2bd9e3c3042394a4
unknown <utkarshup75@gmail.com>
2023-05-08 08:55:39 +0530

    while loop None or Null(js)

---
+++ python/bro_code/while.py
@@ -0,0 +1,11 @@
+# name = ""
+# while len(name) == 0: 
+#     name = input("Enter your name: ") 
+
+# print("Hello " + name)   
+
+name = None 
+while not name: 
+    name = input("Enter your name: ")
+
+print("Hello" + " " + name)
