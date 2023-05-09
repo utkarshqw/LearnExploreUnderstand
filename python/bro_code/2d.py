6f9d97302a418cbe4f58077d678d044fe798231e
unknown <utkarshup75@gmail.com>
2023-05-09 08:58:13 +0530

    2d list

---
+++ python/bro_code/2d.py
@@ -0,0 +1,7 @@
+drinks = ["coffee", "soda", "tea"] 
+dinner = ["pizza", "hamburger", "popcorn" ] 
+dessert = ["cake", "ice cream"] 
+
+food = [drinks, dinner, dessert] 
+print(food)
+print(food[2][2]) 
