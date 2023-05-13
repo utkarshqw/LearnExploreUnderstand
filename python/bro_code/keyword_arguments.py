8eb5c771426c017bb83e3ab2fb4e22aafee35eb5
utkarsh <utkarshup75@gmail.com>
2023-05-13 08:12:08 +0530

    keyword arguments

---
+++ python/bro_code/keyword_arguments.py
@@ -0,0 +1,9 @@
+#positional arguments
+
+def hello(first, middle, last):
+    print("Hello "+ first +" " + middle + " " + last)
+
+hello("code", "Dude", "Bro") 
+
+#keyword arguments 
+hello(last="code", middle="Dude",first="Bro")
